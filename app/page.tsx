c74ea39094321ae0bc780882fa8854310d077a6a
LoveLightRivers <isaacmpm@gmail.com>
2025-05-25 02:19:16 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>TempEmail Pro</h1>
+      <p>Your temporary email service is working!</p>
+    </div>
+  )
+}
